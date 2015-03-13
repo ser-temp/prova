e9be0bdad549ec2196083091e3cdb842e21df92c
Ivan Pik <ivan.pik@basekit.com>
2015-03-13 09:45:26 +0000

    Move scripts to separate file

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+$(function() {
+    FastClick.attach(document.body);
+});
+
+// Makes the whole ecom product tiles clickable
+$(document).ready(function() {
+
+    $('.product-item').each(function() {
+        var href = $(this).find("a").attr("href");
+        if(href) {
+            $(this).addClass("clickable");
+        }
+    });
+
+    $('.product-item').click(function() {
+        var href = $(this).find("a").attr("href");
+        if(href) {
+            window.location = href;
+        }
+    });
+
+    // Adds variation class when there is only one product image
+
+    if($(".ecomproduct__product-gallery").length == 0)
+    {
+       $(".ecomproduct__product-image").addClass("one-image");
+    }
+
+    // Next Button for ecom gallery
+    $('.product-gallery').each(function() {
+      var max = 4
+      if ($(this).find("li.gallery-item").length > max) {
+        $(this).addClass("hiding");
+
+        $(this)
+            .find('li.gallery-item:gt('+max+')')
+            .hide()
+            .end()
+            .append(
+                $('<li class="ecom-view-more" id="ecomscroll" href="#">&gt;</li>').click( function(){
+                $(this).siblings(':hidden').show().end();
+            })
+            );
+      }
+    });
+
+    // Ecom Gallery Scrolling
+
+    $('#ecomscroll').click(function(e) {
+        e.preventDefault();
+        var id = e.target.id;
+        if(id == 'ecomscroll')
+        {
+            $('ul.product-gallery li:first').appendTo('ul.product-gallery');
+        }
+            else
+        {
+            $('ul.product-gallery li:last').prependTo('ul.product-gallery');
+        }
+    });
+
+});
