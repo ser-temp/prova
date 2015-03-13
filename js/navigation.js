2921d681b17ab79065d64e75bafbb6fc431d84e8
Ivan Pik <ivan.pik@basekit.com>
2015-03-13 10:35:07 +0000

    Update nav JS

--- js/navigation.js
+++ js/navigation.js
@@ -1,20 +1,30 @@
 // Toggles class open to display/hide the whole navigation
+
 $( ".custom-navigation-toggle" ).click(function() {
 	$( ".bk-extendednavigation, .drop-down-nav" ).toggleClass( "open" );
 });
 
 // Togglse class open to display/hide folders
+
 $( ".navigation-item.folder" ).each(function() {
 	$( this ).click(function() {
 			$( this ).toggleClass( "open" );
 	});
 });
 
-if (
-    ($("#page-zones__template-widgets__extendednavigation-extendednavigation").length == 0)
-    ||
-    ($("#page-zones__template-widgets__extendednavigation-extendednavigation.bk-tpl-hidden").length == 1)
-)
-{
-   $(".drop-down-nav").addClass("nav-hidden");
-}
+// Hides custom toggle button when navigation is hidden
+
+var hiddenNavHandler = function () {
+    if (
+        ($("#page-zones__template-widgets__extendednavigation-extendednavigation").length == 0)
+        ||
+        ($("#page-zones__template-widgets__extendednavigation-extendednavigation.bk-tpl-hidden").length == 1)
+    )
+    {
+       $(".drop-down-nav").addClass("nav-hidden");
+    }
+};
+
+hiddenNavHandler();
+$(document).ready(hiddenNavHandler);
+$(window).resize(hiddenNavHandler);
